ba25be38acb744c625ad410320247134f93fddb3
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-08-12 19:48:38 +0000

    Added products views and templates

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 # adding an empty path to indicate this is the route url
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.all_products, name='products')
 ]
