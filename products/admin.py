892a82ef1b7f6459304c1cddd3ba6a1f4f42ce26
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-08-12 20:14:08 +0000

    Formatted code

--- products/admin.py
+++ products/admin.py
@@ -3,7 +3,7 @@ from .models import Product, Category
 
 # Register your models here.
 
-
+# re-arranging the order of your products
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
@@ -22,6 +22,6 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
-
+# registering models
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
