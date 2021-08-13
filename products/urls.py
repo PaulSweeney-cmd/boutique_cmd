f7f46bb6daaf8b8642aaf9e7cc91cb70244efa85
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-08-13 20:58:25 +0000

    Addd product details file

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # adding an empty path to indicate this is the route url
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
