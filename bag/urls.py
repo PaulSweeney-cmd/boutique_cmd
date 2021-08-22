d8bd0b11f90938c52d4635df0a9a3e5efd2adfed
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-08-22 12:33:35 +0000

    Added add to bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # adding an empty path to indicate this is the route url
 urlpatterns = [
-    path('', views.view_bag, name='view_bag')
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>', views.add_to_bag, name='add_to_bag')
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,24 @@ def view_bag(request):
     """ A view that renders the shopping bag contents """
 
     return render(request, 'bag/bag.html')
+
+
+# submit the form to the view including the product id and quantity
+def add_to_bag(request, item_id):
+    """ A view to render the bag contents page """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    # geting the bag variable if it exists -
+    # in the session or create if it doesnt
+    bag = request.session.get('bag', {})
+    # update the quantity if it already exsists
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    # add item to the bag
+    else:
+        bag[item_id] = quantity
+    # overwrite the variable in the session with the updated version
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
