5a09ab579dadefd47464d59fb5f717e160b8fcc9
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-09-12 12:25:51 +0000

    Added basic checkout function

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,10 +5,10 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 // set-up stripe by creating a variable using the stripe public key
-var stripe = Stripe(stripe_public_key);
+var stripe = Stripe(stripePublicKey);
 // use stripe to create an instance of elements
 var elements = stripe.elements();
 var style = {
@@ -48,6 +48,43 @@ card.addEventListener('change', function(event) {
     } else {
         errorDiv.text = '';
     }
+});
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    // prevent default action of 'POST'
+    ev.preventDefault();
+    // disable to card element and submit button to prevent multiple submissions
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    /* call the confirm card payment method & 
+    provide the card to styripe then execute the function */
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            /* re-enable the card element and 
+            submit button so user can amend card details */ 
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        /* if the status of the payment intent comes 
+        back as successfull we then submit the form */
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
 
 
