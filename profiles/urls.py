34633ec557b7f9d3b6c6de659bbd68f207b4cd1a
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-09-22 20:10:51 +0000

    Added order history and updated checkout views

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # adding an empty path to indicate this is the route url
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
 ]
